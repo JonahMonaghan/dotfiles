d70fce142b0fa78070e1e60031189158773ad57e
Jonah Monaghan <jonah@jonahmonaghan.com>
2025-11-25 00:59:17 -0500

    Neovim - added lazy.nvim and basic packages

---
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,14 @@
+return {
+	"ibhagwan/fzf-lua",
+	dependencies = {"nvim-mini/mini.icons"},
+	
+	keys = {
+		{"<Leader>pf", "<cmd>FzfLua files<cr>", desc = "Project Files"},
+		{"<Leader>ps", "<cmd>FzfLua live_grep<cr>", desc = "Project Search (Text)"},
+		{"<Leader><space>", "<cmd>FzfLua buffers<cr>", desc = "Switch Open Buffers"},
+		{"<Leader>ph", "<cmd>FzfLua help_tags<cr>", desc = "Project Help"},
+	},
+
+	opts = {
+	}
+}
